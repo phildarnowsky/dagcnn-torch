426386d73d8b5f9ac73c8b64e8c63bfb5bdac986
Phil Darnowsky <phil@darnowsky.com>
2021-05-13 07:49:00 -0400

    Implement average pooling

--- dagcnn_torch.py
+++ dagcnn_torch.py
@@ -3,8 +3,6 @@ from random import choice, randint
 import torch
 from torch import nn
 from torch.nn import functional
-#   CONV1 = auto()
-#   CONV3 = auto()
 #   SEP3 = auto()
 #   SEP5 = auto()
 #   POOL = auto()
@@ -21,6 +19,9 @@ class Node(AutoRepr):
     def to_block(self):
         raise NotImplementedError
 
+    def output_feature_depth(self):
+        raise NotImplementedError
+
     @classmethod
     def arity(cls):
         raise NotImplementedError
@@ -34,18 +35,22 @@ class Block(nn.Module):
         super().__init__()
         self.predecessor_indices = predecessor_indices
         self.output_feature_depth = output_feature_depth
+        self.net = None
 
-    def forward(self, _):
-        raise NotImplementedError
+    def forward(self, input):
+        return self.net(input)
 
 class ConvNode(Node):
     def __init__(self, output_feature_depth, kernel_size):
         super().__init__()
-        self.output_feature_depth = output_feature_depth
+        self.__output_feature_depth = output_feature_depth
         self.kernel_size = kernel_size
 
     def to_block(self, predecessor_indices, input_feature_depth):
-        return ConvBlock(predecessor_indices, input_feature_depth, self.output_feature_depth, self.kernel_size)
+        return ConvBlock(predecessor_indices, input_feature_depth, self.output_feature_depth(), self.kernel_size)
+
+    def output_feature_depth(self):
+        return self.__output_feature_depth
 
     @classmethod
     def arity(cls):
@@ -64,16 +69,41 @@ class ConvBlock(Block):
         conv_layer = nn.Conv2d(input_feature_depth, output_feature_depth, kernel_size, padding=padding)
         relu_layer = nn.ReLU()
         batch_norm_layer = nn.BatchNorm2d(output_feature_depth)
-        self.net = nn.Sequential(conv_layer, relu_layer, batch_norm_layer)
+        self.net = nn.Sequential(conv_layer, relu_layer, batch_norm_layer).cuda()
 
-    def forward(self, input):
-        return self.net(input)
+class AvgPoolNode(Node):
+    def to_block(self, predecessor_indices, input_feature_depth):
+        return AvgPoolBlock(predecessor_indices, input_feature_depth)
+
+    @classmethod
+    def arity(cls):
+        return 1
+
+    @classmethod
+    def make_random(cls):
+        return cls()
+
+class AvgPoolBlock(Block):
+    def __init__(self, predecessor_indices, output_feature_depth):
+        super().__init__(predecessor_indices, output_feature_depth)
+        self.net = nn.AvgPool2d(2, 2).cuda()
 
 class Gene(AutoRepr):
     def __init__(self, node, predecessor_indices):
         assert(node.arity() == len(predecessor_indices))
         self.node = node
         self.predecessor_indices = predecessor_indices
+
+    def to_block(self, model_input_feature_depth, blocks):
+        input_feature_depths = []
+        for predecessor_index in self.predecessor_indices:
+            if predecessor_index == -1:
+                input_feature_depths.append(model_input_feature_depth)
+            else:
+                input_feature_depths.append(blocks[predecessor_index].output_feature_depth)
+        input_feature_depth = max(input_feature_depths)
+        return self.node.to_block(self.predecessor_indices, input_feature_depth)
+
 
 class Genome(AutoRepr):
     def __init__(self, input_feature_depth, output_feature_depth, genes):
@@ -86,20 +116,21 @@ class Genome(AutoRepr):
         output_indices = set(range(len(self.genes)))
 
         for gene in self.genes:
-            input_feature_depth = self.__input_feature_depth(gene)
-            block = gene.node.to_block(gene.predecessor_indices, input_feature_depth)
+#           input_feature_depth = self.__input_feature_depth(gene)
+#           block = gene.node.to_block(gene.predecessor_indices, input_feature_depth)
+            block = gene.to_block(self.input_feature_depth, blocks)
             blocks.append(block)
             output_indices = output_indices.difference(set(gene.predecessor_indices))
         return Individual(blocks, output_indices, self.output_feature_depth)
 
-    def __input_feature_depth(self, gene):
-        input_feature_depths = []
-        for predecessor_index in gene.predecessor_indices:
-            if predecessor_index == -1:
-                input_feature_depths.append(self.input_feature_depth)
-            else:
-                input_feature_depths.append(self.genes[predecessor_index].node.output_feature_depth)
-        return max(input_feature_depths)
+#   def __input_feature_depth(self, gene):
+#       input_feature_depths = []
+#       for predecessor_index in gene.predecessor_indices:
+#           if predecessor_index == -1:
+#               input_feature_depths.append(self.input_feature_depth)
+#           else:
+#               input_feature_depths.append(self.genes[predecessor_index].node.output_feature_depth())
+#       return max(input_feature_depths)
 
     @classmethod
     def make_random(cls, input_feature_depth, output_feature_depth, min_length, max_length):
@@ -120,7 +151,7 @@ class Genome(AutoRepr):
 
     @classmethod
     def __instantiable_classes(cls):
-        return [ConvNode]
+        return [ConvNode, AvgPoolNode]
 
 class Individual(nn.Module, AutoRepr):
     def __init__(self, blocks, output_indices, output_feature_depth, final_layer = nn.Identity()):
@@ -164,13 +195,15 @@ class Individual(nn.Module, AutoRepr):
         max_height = max(heights)
         result = []
         for tensor in tensors:
-            channel_padding = (max_feature_depth - tensor.size(1)) // 2
+            channel_difference = max_feature_depth - tensor.size(1)
+            channel_padding_front = channel_difference // 2
+            channel_padding_back = channel_difference - channel_padding_front
             height_and_width_padding = (max_height - tensor.size(2)) // 2
-            padding = (height_and_width_padding, height_and_width_padding, height_and_width_padding, height_and_width_padding, channel_padding, channel_padding)
+            padding = (height_and_width_padding, height_and_width_padding, height_and_width_padding, height_and_width_padding, channel_padding_front, channel_padding_back)
             padded_tensor = functional.pad(tensor, padding)
             result.append(padded_tensor)
         return result
 
     def __make_tail(self, input_feature_depth):
-        fc_layer = nn.Linear(input_feature_depth, self.output_feature_depth)
-        self.tail = nn.Sequential(fc_layer, self.final_layer)
+        fc_layer = nn.Linear(input_feature_depth, self.output_feature_depth).cuda()
+        self.tail = nn.Sequential(fc_layer, self.final_layer).cuda()
