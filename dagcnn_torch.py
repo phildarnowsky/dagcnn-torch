1c646383044a7a4edbc195797ce5983dbf351614
Phil Darnowsky <phil@darnowsky.com>
2021-05-16 12:29:28 -0400

    Implement CatNode and CatBlock

--- dagcnn_torch.py
+++ dagcnn_torch.py
@@ -14,7 +14,7 @@ class Node(AutoRepr):
     def __init__(self, input_feature_depths):
         self.input_feature_depths = input_feature_depths
 
-    def to_block(self):
+    def to_block(self, _):
         raise NotImplementedError
 
     def output_feature_depth(self, _):
@@ -25,14 +25,13 @@ class Node(AutoRepr):
         raise NotImplementedError
 
     @classmethod
-    def make_random(cls):
+    def make_random(cls, _):
         raise NotImplementedError
 
 class Block(nn.Module):
-    def __init__(self, input_indices, output_feature_depth):
+    def __init__(self, input_indices):
         super().__init__()
         self.input_indices = input_indices
-        self.output_feature_depth = output_feature_depth
         self.net = None
 
     def forward(self, input):
@@ -62,7 +61,7 @@ class ConvNode(Node):
 
 class ConvBlock(Block):
     def __init__(self, input_indices, input_feature_depth, output_feature_depth, kernel_size):
-        super().__init__(input_indices, output_feature_depth)
+        super().__init__(input_indices)
         padding = kernel_size // 2
         conv_layer = nn.Conv2d(input_feature_depth, output_feature_depth, kernel_size, padding=padding)
         relu_layer = nn.ReLU()
@@ -74,7 +73,7 @@ class AvgPoolNode(Node):
         return max(input_feature_depths)
 
     def to_block(self, input_indices):
-        return AvgPoolBlock(input_indices, self.input_feature_depths)
+        return AvgPoolBlock(input_indices)
 
     @classmethod
     def arity(cls):
@@ -85,11 +84,30 @@ class AvgPoolNode(Node):
         return cls(input_feature_depths)
 
 class AvgPoolBlock(Block):
-    def __init__(self, input_indices, input_feature_depths):
-        output_feature_depth = max(input_feature_depths)
-        super().__init__(input_indices, output_feature_depth)
+    def __init__(self, input_indices):
+        super().__init__(input_indices)
         self.net = nn.AvgPool2d(2, 2).cuda()
 
+class CatNode(Node):
+    def to_block(self, input_indices):
+        return CatBlock(input_indices)
+
+    def output_feature_depth(self, input_feature_depths):
+        return sum(input_feature_depths)
+
+    @classmethod
+    def arity(cls):
+        return 2
+
+    @classmethod
+    def make_random(cls, input_feature_depths):
+        return cls(input_feature_depths)
+
+class CatBlock(Block):
+    def forward(self, input1, input2):
+        [padded_input1, padded_input2] = match_shapes([input1, input2], match_channels=False)
+        return torch.cat((padded_input1, padded_input2), 1)
+        
 class Gene(AutoRepr):
     def __init__(self, node, input_indices, input_feature_depths):
         assert(node.arity() == len(input_indices))
@@ -102,7 +120,6 @@ class Gene(AutoRepr):
 
     def to_block(self):
         return self.node.to_block(self.input_indices)
-
 
 class Genome(AutoRepr):
     def __init__(self, input_feature_depth, output_feature_depth, genes):
@@ -145,7 +162,7 @@ class Genome(AutoRepr):
 
     @classmethod
     def __instantiable_classes(cls):
-        return [ConvNode, AvgPoolNode]
+        return [ConvNode, AvgPoolNode, CatNode]
 
 class Individual(nn.Module, AutoRepr):
     def __init__(self, blocks, output_indices, output_feature_depth, final_layer = nn.Identity()):
@@ -163,12 +180,13 @@ class Individual(nn.Module, AutoRepr):
             result = block(*inputs)
             results.append(result)
         output_results = list(map(lambda output_index: results[output_index], self.output_indices))
-        output_results = self.__match_shapes(output_results)
+        output_results = match_shapes(output_results)
         output = torch.zeros_like(output_results[0])
         for output_result in output_results:
             output += output_result
         output = torch.flatten(output, 1)
         if self.tail == None:
+            print(f"Attempting to make tail of size {output.size(1)}")
             self.__make_tail(output.size(1))
         output = self.tail(output)
         return output
@@ -182,38 +200,38 @@ class Individual(nn.Module, AutoRepr):
                 inputs.append(results[input_index])
         return inputs
 
-    def __match_shapes(self, tensors, match_channels=True):
-        feature_depths = map(lambda tensor: tensor.size(1), tensors)
-        heights = map(lambda tensor: tensor.size(2), tensors)
-        widths = map(lambda tensor: tensor.size(3), tensors)
-        max_feature_depth = max(feature_depths)
-        max_height = max(heights)
-        max_width = max(widths)
-        result = []
-        for tensor in tensors:
-            if match_channels:
-                channel_difference = max_feature_depth - tensor.size(1)
-            else:
-                channel_difference = 0
-            channel_padding_front = channel_difference // 2
-            channel_padding_back = channel_difference - channel_padding_front
-            
-            height_difference = max_height - tensor.size(2)
-            height_padding_top = height_difference // 2
-            height_padding_bottom = height_difference - height_padding_top
-            
-            width_difference = max_width - tensor.size(3)
-            width_padding_left = width_difference // 2
-            width_padding_right = width_difference - width_padding_left
-
-            padding = (width_padding_left, width_padding_right, height_padding_top, height_padding_bottom, channel_padding_front, channel_padding_back)
-            padded_tensor = functional.pad(tensor, padding)
-            result.append(padded_tensor)
-        return result
-
     def __make_tail(self, input_feature_depth):
         fc_layer = nn.Linear(input_feature_depth, self.output_feature_depth).cuda()
         self.tail = nn.Sequential(fc_layer, self.final_layer).cuda()
+
+def match_shapes(tensors, match_channels=True):
+    feature_depths = map(lambda tensor: tensor.size(1), tensors)
+    heights = map(lambda tensor: tensor.size(2), tensors)
+    widths = map(lambda tensor: tensor.size(3), tensors)
+    max_feature_depth = max(feature_depths)
+    max_height = max(heights)
+    max_width = max(widths)
+    result = []
+    for tensor in tensors:
+        if match_channels:
+            channel_difference = max_feature_depth - tensor.size(1)
+        else:
+            channel_difference = 0
+        channel_padding_front = channel_difference // 2
+        channel_padding_back = channel_difference - channel_padding_front
+        
+        height_difference = max_height - tensor.size(2)
+        height_padding_top = height_difference // 2
+        height_padding_bottom = height_difference - height_padding_top
+        
+        width_difference = max_width - tensor.size(3)
+        width_padding_left = width_difference // 2
+        width_padding_right = width_difference - width_padding_left
+
+        padding = (width_padding_left, width_padding_right, height_padding_top, height_padding_bottom, channel_padding_front, channel_padding_back)
+        padded_tensor = functional.pad(tensor, padding)
+        result.append(padded_tensor)
+    return result
 
 if __name__ == "__main__":
     data = torch.randn(1, 1, 320, 320).cuda()
