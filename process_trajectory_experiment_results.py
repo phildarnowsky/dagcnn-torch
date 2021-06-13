d9acddb6e9108281cf615502088f9889bfe38cc9
Phil Darnowsky <phil@darnowsky.com>
2021-06-13 15:04:22 -0400

    Add annotation and marker to validation plots

--- process_trajectory_experiment_results.py
+++ process_trajectory_experiment_results.py
@@ -3,10 +3,10 @@ import seaborn as sns
 
 genome_length_to_filename = {
     5: "./experiment_results/trajectory_2021-06-11T22:12:25.530587.csv",
-#   10: "./experiment_results/trajectory_2021-06-11T16:28:30.262541.csv",
-#   15: "./experiment_results/trajectory_2021-06-11T08:00:50.091424.csv",
-#   20: "./experiment_results/trajectory_2021-06-10T16:00:53.545725.csv",
-#   25: "./experiment_results/trajectory_2021-06-09T19:19:02.479717.csv"
+    10: "./experiment_results/trajectory_2021-06-11T16:28:30.262541.csv",
+    15: "./experiment_results/trajectory_2021-06-11T08:00:50.091424.csv",
+    20: "./experiment_results/trajectory_2021-06-10T16:00:53.545725.csv",
+    25: "./experiment_results/trajectory_2021-06-09T19:19:02.479717.csv"
 }
 
 genome_length_to_data_frame = {length : pd.read_csv(filename) for length, filename in genome_length_to_filename.items()}
@@ -26,8 +26,16 @@ for genome_length, df in genome_length_to_data_frame.items():
     evolution_output_filename = f"./experiment_results/plots/average_evolution_loss_length_{genome_length}.pdf"
     validation_output_filename = f"./experiment_results/plots/average_validation_loss_length_{genome_length}.pdf"
 
-    evolution_plot = sns.relplot(kind="line", y=evolution_average_losses, x=range(1, 101))
+    evolution_plot = sns.relplot(kind="line", y=evolution_average_losses, x=range(1, 101), aspect=2.0).set_axis_labels("Number of epochs of training", "Average loss")
+    evolution_plot.tight_layout()
     evolution_plot.fig.savefig(evolution_output_filename)
 
-    validation_plot = sns.relplot(kind="line", y=validation_average_losses, x=range(1, 101))
+    validation_plot = sns.relplot(kind="line", y=validation_average_losses, x=range(1, 101), aspect=2.0).set_axis_labels("Number of epochs of training", "Average loss")
+    axes = validation_plot.axes[0][0]
+    min_x = validation_average_losses.argmin() + 1
+    min_y = validation_average_losses.min()
+    axes.plot(min_x, min_y, 'ro')
+    annotation_text = "Minimum: ({min_x}, {min_y:.3f})".format(min_x=min_x, min_y=min_y)
+    axes.annotate(annotation_text, (min_x, min_y), xytext=(min_x + 3, min_y + 0.2), arrowprops={'arrowstyle': '->'})
+    validation_plot.tight_layout()
     validation_plot.fig.savefig(validation_output_filename)
