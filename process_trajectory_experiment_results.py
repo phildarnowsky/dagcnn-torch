2f3901eaf19bbbfe5edd0db126f47cf48010e084
Phil Darnowsky <phil@darnowsky.com>
2021-06-14 17:41:18 -0400

    Plot all lengths on single graph

--- process_trajectory_experiment_results.py
+++ process_trajectory_experiment_results.py
@@ -12,6 +12,12 @@ genome_length_to_filename = {
 
 genome_length_to_data_frame = {length : pd.read_csv(filename) for length, filename in genome_length_to_filename.items()}
 
+evolution_average_loss_result = pd.DataFrame()
+validation_average_loss_result = pd.DataFrame()
+rho_en_vn_result = pd.DataFrame()
+rho_en_vm_result = pd.DataFrame()
+rho_vn_vm_result = pd.DataFrame()
+
 for genome_length, df in genome_length_to_data_frame.items():
     evolution_losses = df.loc[
        :,
@@ -22,27 +28,14 @@ for genome_length, df in genome_length_to_data_frame.items():
       'Validation loss after 1 epochs':'Validation loss after 100 epochs'
     ]
     evolution_average_losses = evolution_losses.mean(axis=0)
+    evolution_df = pd.DataFrame({"loss": evolution_average_losses, "n_epochs": range(1, 101), "genome_length": genome_length})
+    evolution_average_loss_result = pd.concat([evolution_average_loss_result, evolution_df])
+
     validation_average_losses = validation_losses.mean(axis=0)
-
-    evolution_output_filename = f"./experiment_results/plots/average_evolution_loss_length_{genome_length}.pdf"
-    validation_output_filename = f"./experiment_results/plots/average_validation_loss_length_{genome_length}.pdf"
-    rho_en_vn_output_filename = f"./experiment_results/plots/rho_en_vn_{genome_length}.pdf"
-    rho_en_vm_output_filename = f"./experiment_results/plots/rho_en_vm_{genome_length}.pdf"
-    rho_vn_vm_output_filename = f"./experiment_results/plots/rho_vn_vm_{genome_length}.pdf"
-
-    evolution_plot = sns.relplot(kind="line", y=evolution_average_losses, x=range(1, 101), aspect=2.0).set_axis_labels("Number of epochs of training", "Average loss")
-    evolution_plot.tight_layout()
-    evolution_plot.fig.savefig(evolution_output_filename)
-
-    validation_plot = sns.relplot(kind="line", y=validation_average_losses, x=range(1, 101), aspect=2.0).set_axis_labels("Number of epochs of training", "Average loss")
-    axes = validation_plot.axes[0][0]
     min_x = validation_average_losses.argmin() + 1
     min_y = validation_average_losses.min()
-    axes.plot(min_x, min_y, 'ro')
-    annotation_text = "Minimum: ({min_x}, {min_y:.3f})".format(min_x=min_x, min_y=min_y)
-    axes.annotate(annotation_text, (min_x, min_y), xytext=(min_x + 3, min_y + 0.2), arrowprops={'arrowstyle': '->'})
-    validation_plot.tight_layout()
-    validation_plot.fig.savefig(validation_output_filename)
+    validation_df = pd.DataFrame({"loss": validation_average_losses, "n_epochs": range(1, 101), "genome_length": genome_length})
+    validation_average_loss_result = pd.concat([validation_average_loss_result, validation_df])
 
     evolution_losses_before_overfit = evolution_losses.loc[
        :,
@@ -52,27 +45,49 @@ for genome_length, df in genome_length_to_data_frame.items():
        :,
        'Validation loss after 1 epochs':f"Validation loss after {min_x} epochs"
     ]
+
     rho_en_vn = map(
         lambda n: evolution_losses_before_overfit.iloc[:, n].corr(validation_losses_before_overfit.iloc[:, n]),
         range(0, min_x)
     )
+    rho_en_vn_df = pd.DataFrame({"rho": rho_en_vn, "n_epochs": range(1, min_x + 1), "genome_length": genome_length})
+    rho_en_vn_result = pd.concat([rho_en_vn_result, rho_en_vn_df])
+
     rho_en_vm = map(
         lambda n: evolution_losses_before_overfit.iloc[:, n].corr(validation_losses_before_overfit.iloc[:, -1]),
         range(0, min_x)
     )
+    rho_en_vm_df = pd.DataFrame({"rho": rho_en_vm, "n_epochs": range(1, min_x + 1), "genome_length": genome_length})
+    rho_en_vm_result = pd.concat([rho_en_vm_result, rho_en_vm_df])
+
     rho_vn_vm = map(
         lambda n: validation_losses_before_overfit.iloc[:, n].corr(validation_losses_before_overfit.iloc[:, -1]),
         range(0, min_x)
     )
+    rho_vn_vm_df = pd.DataFrame({"rho": rho_vn_vm, "n_epochs": range(1, min_x + 1), "genome_length": genome_length})
+    rho_vn_vm_result = pd.concat([rho_vn_vm_result, rho_vn_vm_df])
 
-    rho_en_vn_plot = sns.relplot(kind="line", y=list(rho_en_vn), x=range(1, min_x + 1), aspect=2.0).set_axis_labels("Number of epochs of training", "\u03c1(en, vn)")
-    rho_en_vn_plot.tight_layout()
-    rho_en_vn_plot.fig.savefig(rho_en_vn_output_filename)
+evolution_plot = sns.relplot(kind="line", data=evolution_average_loss_result, x="n_epochs", y="loss", hue="genome_length").set_axis_labels("Number of epochs of training", "Average loss")
+evolution_plot.legend.set_title("Genome length")
+evolution_plot.tight_layout()
+evolution_plot.fig.savefig("experiment_results/plots/average_evolution_loss.pdf")
 
-    rho_en_vm_plot = sns.relplot(kind="line", y=list(rho_en_vm), x=range(1, min_x + 1), aspect=2.0).set_axis_labels("Number of epochs of training", "\u03c1(en, vm)")
-    rho_en_vm_plot.tight_layout()
-    rho_en_vm_plot.fig.savefig(rho_en_vm_output_filename)
- 
-    rho_vn_vm_plot = sns.relplot(kind="line", y=list(rho_vn_vm), x=range(1, min_x + 1), aspect=2.0).set_axis_labels("Number of epochs of training", "\u03c1(vn, vm)")
-    rho_vn_vm_plot.tight_layout()
-    rho_vn_vm_plot.fig.savefig(rho_vn_vm_output_filename)
+validation_plot = sns.relplot(kind="line", data=validation_average_loss_result, x="n_epochs", y="loss", hue="genome_length").set_axis_labels("Number of epochs of training", "Average loss")
+validation_plot.legend.set_title("Genome length")
+validation_plot.tight_layout()
+validation_plot.fig.savefig("experiment_results/plots/average_validation_loss.pdf")
+
+rho_en_vn_plot = sns.relplot(kind="line", data=rho_en_vn_result, x="n_epochs", y="rho", hue="genome_length").set_axis_labels("Number of epochs of training", "\u03c1(en, vn)")
+rho_en_vn_plot.legend.set_title("Genome length")
+rho_en_vn_plot.tight_layout()
+rho_en_vn_plot.fig.savefig("experiment_results/plots/rho_en_vn.pdf")
+
+rho_en_vm_plot = sns.relplot(kind="line", data=rho_en_vm_result, x="n_epochs", y="rho", hue="genome_length").set_axis_labels("Number of epochs of training", "\u03c1(en, vm)")
+rho_en_vm_plot.legend.set_title("Genome length")
+rho_en_vm_plot.tight_layout()
+rho_en_vm_plot.fig.savefig("experiment_results/plots/rho_en_vm.pdf")
+
+rho_vn_vm_plot = sns.relplot(kind="line", data=rho_vn_vm_result, x="n_epochs", y="rho", hue="genome_length").set_axis_labels("Number of epochs of training", "\u03c1(vn, vm)")
+rho_vn_vm_plot.legend.set_title("Genome length")
+rho_vn_vm_plot.tight_layout()
+rho_vn_vm_plot.fig.savefig("experiment_results/plots/rho_vn_vm.pdf")
