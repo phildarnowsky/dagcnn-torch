cf18d7985ea1d2533e8eb88c1fe23ef3522f6cd6
Phil Darnowsky <phil@darnowsky.com>
2021-06-22 14:44:37 -0400

    be consistent about using _ for private fields

--- dagcnn_torch.py
+++ dagcnn_torch.py
@@ -15,24 +15,24 @@ class AutoRepr():
 
 class Node(AutoRepr):
     def __init__(self, input_shapes):
-        self.input_shapes = input_shapes
+        self._input_shapes = input_shapes
 
     def to_block(self, _):
         raise NotImplementedError
 
     def to_cache_key(self):
         return "_".join([
-            self.cache_node_type(),
-            ",".join(map(str, self.cache_parameters()))
+            self._cache_node_type(),
+            ",".join(map(str, self._cache_parameters()))
         ])
 
     def output_shape(self, _):
         raise NotImplementedError
 
-    def cache_node_type(self):
+    def _cache_node_type(self):
         raise NotImplementedError
 
-    def cache_parameters(self):
+    def _cache_parameters(self):
         raise NotImplementedError
 
     @classmethod
@@ -47,15 +47,15 @@ class Block(nn.Module):
     def __init__(self, input_indices):
         super().__init__()
         self.input_indices = input_indices
-        self.net = None
+        self._net = None
 
     def forward(self, input):
-        return self.net(input)
+        return self._net(input)
 
 class ConvNode(Node):
     def __init__(self, input_shapes, output_feature_depth, kernel_size):
         super().__init__(input_shapes)
-        self._input_shape = self.input_shapes[0]
+        self._input_shape = self._input_shapes[0]
         self._output_feature_depth = output_feature_depth
         self._kernel_size = kernel_size
 
@@ -65,10 +65,10 @@ class ConvNode(Node):
     def output_shape(self, _):
         return (self._output_feature_depth, self._input_shape[1], self._input_shape[2]) 
 
-    def cache_node_type(self):
+    def _cache_node_type(self):
         return "C"
 
-    def cache_parameters(self):
+    def _cache_parameters(self):
         return [self._kernel_size, self._output_feature_depth]
 
     @classmethod
@@ -89,12 +89,12 @@ class ConvBlock(Block):
         kaiming_normal_(conv_layer.weight)
         relu_layer = nn.ReLU()
         batch_norm_layer = nn.BatchNorm2d(output_feature_depth)
-        self.net = nn.Sequential(conv_layer, relu_layer, batch_norm_layer).cuda()
+        self._net = nn.Sequential(conv_layer, relu_layer, batch_norm_layer).cuda()
 
 class DepSepConvNode(Node):
     def __init__(self, input_feature_depths, output_feature_depth, kernel_size):
         super().__init__(input_feature_depths)
-        self._input_shape = self.input_shapes[0]
+        self._input_shape = self._input_shapes[0]
         self._output_feature_depth = output_feature_depth
         self._kernel_size = kernel_size
 
@@ -104,10 +104,10 @@ class DepSepConvNode(Node):
     def output_shape(self, _):
         return (self._output_feature_depth, self._input_shape[1], self._input_shape[2]) 
 
-    def cache_node_type(self):
+    def _cache_node_type(self):
         return "D"
 
-    def cache_parameters(self):
+    def _cache_parameters(self):
         return [self._kernel_size, self._output_feature_depth]
 
     @classmethod
@@ -130,23 +130,23 @@ class DepSepConvBlock(Block):
         kaiming_normal_(pointwise_layer.weight)
         relu_layer = nn.ReLU()
         batch_norm_layer = nn.BatchNorm2d(output_feature_depth)
-        self.net = nn.Sequential(depthwise_layer, pointwise_layer, relu_layer, batch_norm_layer).cuda()
+        self._net = nn.Sequential(depthwise_layer, pointwise_layer, relu_layer, batch_norm_layer).cuda()
 
 class PoolNode(Node):
-    def is_too_small(self, shape):
+    def _is_too_small(self, shape):
         return shape[1] < 2 or shape[2] < 2
 
     def output_shape(self, input_shapes):
         input_shape = input_shapes[0]
-        if self.is_too_small(input_shape):
+        if self._is_too_small(input_shape):
             return input_shape
         else:
             return input_shape[0], input_shape[1] // 2, input_shape[2] // 2
 
     def to_block(self, input_indices):
-        return self.block_class()(input_indices, self.is_too_small(self.input_shapes[0]))
+        return self._block_class()(input_indices, self._is_too_small(self._input_shapes[0]))
 
-    def block_class(self):
+    def _block_class(self):
         raise NotImplementedError
 
     @classmethod
@@ -161,39 +161,39 @@ class PoolBlock(Block):
     def __init__(self, input_indices, is_too_small):
         super().__init__(input_indices)
         if is_too_small:
-            self.net = nn.Identity().cuda()
+            self._net = nn.Identity().cuda()
         else:
-            self.net = self.layer_class()(2, 2).cuda()
+            self._net = self._layer_class()(2, 2).cuda()
 
-    def layer_class(self):
+    def _layer_class(self):
         raise NotImplementedError
 
 class AvgPoolNode(PoolNode):
-    def block_class(self):
+    def _block_class(self):
         return AvgPoolBlock
 
-    def cache_node_type(self):
+    def _cache_node_type(self):
         return "A"
 
-    def cache_parameters(self):
+    def _cache_parameters(self):
         return []
 
 class AvgPoolBlock(PoolBlock):
-    def layer_class(self):
+    def _layer_class(self):
         return nn.AvgPool2d
 
 class MaxPoolNode(PoolNode):
-    def block_class(self):
+    def _block_class(self):
         return MaxPoolBlock
 
-    def cache_node_type(self):
+    def _cache_node_type(self):
         return "M"
 
-    def cache_parameters(self):
+    def _cache_parameters(self):
         return []
 
 class MaxPoolBlock(PoolBlock):
-    def layer_class(self):
+    def _layer_class(self):
         return nn.MaxPool2d
 
 class CatNode(Node):
@@ -206,10 +206,10 @@ class CatNode(Node):
         output_width = max(map(lambda t: t[0], input_shapes))
         return (output_feature_depth, output_height, output_width)
 
-    def cache_node_type(self):
+    def _cache_node_type(self):
         return "K"
 
-    def cache_parameters(self):
+    def _cache_parameters(self):
         return []
 
     @classmethod
@@ -235,10 +235,10 @@ class SumNode(Node):
         output_width = max(map(lambda t: t[0], input_shapes))
         return (output_feature_depth, output_height, output_width)
 
-    def cache_node_type(self):
+    def _cache_node_type(self):
         return "S"
 
-    def cache_parameters(self):
+    def _cache_parameters(self):
         return []
 
     @classmethod
@@ -259,10 +259,10 @@ class Gene(AutoRepr):
         assert(node.arity() == len(input_indices))
         self.node = node
         self.input_indices = input_indices
-        self.input_shapes = input_shapes
+        self._input_shapes = input_shapes
 
     def output_shape(self):
-        return self.node.output_shape(self.input_shapes)
+        return self.node.output_shape(self._input_shapes)
 
     def to_block(self):
         return self.node.to_block(self.input_indices)
@@ -405,7 +405,7 @@ def match_shapes(tensors, match_channels=True):
 class Population():
     def __init__(self, genomes):
         self.genomes = genomes
-        self.fitness_cache = {}
+        self._fitness_cache = {}
 
     @classmethod
     def make_random(cls, n_genomes, input_shape, n_outputs, minimum_length, maximum_length):
