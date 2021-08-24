24dcfbf5f3061bd78fed3eaba2787d3931aca8c4
Phil Darnowsky <phil@darnowsky.com>
2021-08-24 14:59:29 -0400

    Second final results from experiment 3 (take 2)

--- process_cifar_classifier_experiment_3_results.sh
+++ process_cifar_classifier_experiment_3_results.sh
@@ -1,1 +1,1 @@
-python process_cifar_classifier_experiment_results.py ./experiment_results/tables/cifar_10_results_run_3.tex ./experiment_results/cifar_10_classifier_accuracy_2021-08-24T12:44:05.926731.pickle
+python process_cifar_classifier_experiment_results.py ./experiment_results/tables/cifar_10_results_run_3.tex ./experiment_results/cifar_10_classifier_accuracy_2021-08-24T12:44:05.926731.pickle ./experiment_results/cifar_10_classifier_accuracy_2021-08-24T14:53:18.972372.pickle ./experiment_results/cifar_10_classifier_accuracy_2021-08-24T14:56:20.406000.pickle
