7be6832e84995bd80c902c4589d1fc028f8b0008
Phil Darnowsky <phil@darnowsky.com>
2021-09-08 19:18:24 -0400

    tweak census

--- census.py
+++ census.py
@@ -1,3 +1,4 @@
+import math
 import os
 import pickle
 import re
@@ -5,22 +6,34 @@ import sys
 
 input_filenames = sys.argv[1:]
 genomes = []
-results = {}
+accuracies = {}
+results = {'A': 0, 'C': 0, 'D': 0, 'K': 0, 'M': 0, 'S': 0}
 
 for filename in input_filenames:
     with open(filename, "rb") as f:
-        file_genomes = list(pickle.load(f, encoding='bytes')['accuracies'].keys())
-        print(file_genomes)
-    genomes = genomes + file_genomes
+        file_accuracies = pickle.load(f, encoding='bytes')['accuracies']
+    for genome in file_accuracies:
+        genomes.append(genome)
+        accuracies[genome] = max(file_accuracies[genome].values())
 
-for genome in genomes:
+genomes = sorted(genomes, key=lambda x: -accuracies[x])
+n_genomes_to_keep = math.floor(len(genomes) * 0.1)
+
+print(n_genomes_to_keep)
+for genome in genomes[:n_genomes_to_keep]:
     genes = re.split('(?<=\))', genome)
     genes.pop()
     for gene in genes:
         gene = re.sub('[\(\)]', "", gene)
         operation = re.split('_', gene)[0]
-        if operation not in results.keys():
-            results[operation] = 0
         results[operation] += 1
+
+total_nodes = sum(results.values())
+
+for operation in sorted(results):
+    n_nodes = results[operation]
+    percentage = n_nodes * 1.0 / total_nodes
+    print(f"{operation}: {percentage:.4f} {n_nodes}")
+print(total_nodes)
 
 print(results)
