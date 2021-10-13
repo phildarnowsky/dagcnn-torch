c86717888ad874546dfbdd4bd97bc62ef47f87c4
Phil Darnowsky <phil@darnowsky.com>
2021-10-13 07:37:40 -0400

    Second final results from experiment 11

--- evaluate_cifar_run_11.sh
+++ evaluate_cifar_run_11.sh
@@ -1,1 +1,1 @@
-time python evaluate_cifar_10_classifiers.py experiment_results/cifar_10_classifier_2021-09-25T11:48:40.767411.pickle experiment_results/cifar_10_classifier_2021-09-26T08:04:46.863118.pickle experiment_results/cifar_10_classifier_2021-09-27T02:13:14.947593.pickle
+time python evaluate_cifar_10_classifiers.py  experiment_results/cifar_10_classifier_2021-09-26T08:04:46.863118.pickle experiment_results/cifar_10_classifier_2021-09-27T02:13:14.947593.pickle
